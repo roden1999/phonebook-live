339a27c25df3aeef3ebee1234ca9a1ad21d583ba
Roden <rodenmontoya5@gmail.com>
2020-07-04 00:39:00 +0800

    change MEDIA_URL path

--- phonebook/settings.py
+++ phonebook/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/App_Data/'
+MEDIA_URL = '/static/App_Data/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
