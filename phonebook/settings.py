3c5842c7cf0d9b4fd8260732a1ac7f3657e6521a
Roden <rodenmontoya5@gmail.com>
2020-07-16 11:59:57 +0800

    change db

--- phonebook/settings.py
+++ phonebook/settings.py
@@ -143,5 +143,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/App_Data/images')
 
 # CORS_ORIGIN_WHITELIST = (
-#     'http//:localhost:3000',
+#     # 'http//:localhost:3000',
+#     'http://127.0.0.1:8000',
+#     'https://roden-covidproject-phonebook.herokuapp.com',
 # )
